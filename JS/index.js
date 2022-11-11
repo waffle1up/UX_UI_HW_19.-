05f16418c9841012e183f0a093aaa0b60934c54b
waffle1up <cgbadehan@gmail.com>
2022-11-10 19:24:01 -0500

    jjjj

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,11 @@
-console.log ("Your index.js file is loaded correctly!");
-$("#downloadMyResume").on("click",function(){
-    console.log("You clicked on #downloadMyResume");
-});
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if(this.hash !==""){
+            event.preventDefault();
+            var hash = this.hash;
+            $('html,body').animate ({
+                scrollTop: $(hash).offset().top}, 400, function() { window.location.hash = hash; 
+                });
+            } 
+          });
+        });         
